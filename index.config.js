20fe374c6babe69c1dc46247c671762306c17151
HostingSolutionTime <lukepluto05+githubalt@gmail.com>
2026-05-09 01:02:16 -0700

    Add files via upload

---
+++ index.config.js
@@ -0,0 +1,61 @@
+window.SPLASH_CONFIG = window.SPLASH_CONFIG || {
+  wisp: {
+    default: "wss://stienish-schools.eminescusm.ro/wisp/",
+    allowInject: true,
+  },
+  head: {
+    title: "SPLASH",
+    favicon: "/favicon-32x32.png",
+    meta: [
+      {
+        name: "title",
+        content: "SPLASH",
+      },
+      {
+        name: "description",
+        content: "some resources for the ap exam",
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:url",
+        content: "https://splash.rhw.one/",
+      },
+      {
+        property: "og:title",
+        content: "SPLASH",
+      },
+      {
+        property: "og:description",
+        content: "some resources for the ap exam",
+      },
+      {
+        property: "og:image",
+        content: "https://splash.rhw.one/card.png",
+      },
+      {
+        property: "twitter:card",
+        content: "summary_large_image",
+      },
+      {
+        property: "twitter:url",
+        content: "https://splash.rhw.one/",
+      },
+      {
+        property: "twitter:title",
+        content: "SPLASH",
+      },
+      {
+        property: "twitter:description",
+        content: "some resources for the ap exam",
+      },
+      {
+        property: "twitter:image",
+        content: "https://splash.rhw.one/card.png",
+      },
+    ],
+  },
+};
+
